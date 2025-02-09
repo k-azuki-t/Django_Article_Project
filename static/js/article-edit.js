61596b146e300dc87a3228a724145728381df331
k-azuki-t <teraoctit@gmail.com>
2025-02-09 17:08:41 +0900

    作成ボタンのスタイル修正

---
+++ static/js/article-edit.js
@@ -0,0 +1,68 @@
+const dropArea = document.querySelector('.drop-area');
+const fileInput = document.getElementById('id_header_img_url');
+
+// 添付した画像ファイルを表示する関数
+function handleFile(imgFIle) {
+    const reader = new FileReader();
+
+    reader.readAsDataURL(imgFIle);
+    reader.onload = (e) => {
+        // 画像がすでに表示されている場合は削除
+        if (dropArea.querySelector('img')) {
+            dropArea.querySelector('img').remove();
+        }
+        // フォームに選択した画像を表示
+        const img = document.createElement('img');
+        img.src = e.target.result;
+        img.alt = 'Uploaded Image';
+        img.style.objectFit = 'cover';
+        img.style.width = '100%';
+        img.style.height = '100%';
+        dropArea.appendChild(img);
+        dropArea.querySelector('.img-form-message').style.display = 'none';
+    }
+
+    // 画像フォーム内の要素を非表示にする
+    dropArea.querySelector('.bi-image').style.display = 'none';
+    dropArea.querySelector('#id_header_img_url').style.display = 'none';
+}
+
+// エクスプローラーでのファイル選択時の処理
+fileInput.addEventListener('change', (e) => {
+    // 記事編集画面でのみ処理を実行
+    if (window.location.pathname == '/articles/edit/') {
+        const imgFile = e.target.files[0];
+        if (imgFile && imgFile.type.startsWith('image/')) {
+            handleFile(imgFile);
+        } else {
+            fileInput.value = '';
+            alert('画像ファイルを選択してください');
+        }
+    }
+});
+
+// ドラッグ&ドロップ時の処理
+dropArea.addEventListener('drop', (e) => {
+    if (window.location.pathname == '/articles/edit/') {
+
+        // input要素にファイルをセットするためのDataTransferオブジェクトを作成
+        const imgFile = e.dataTransfer.files[0];
+        const dataTransfer = new DataTransfer();
+
+        // 選択したファイルが画像ファイルかどうかを判定
+        if (imgFile && imgFile.type.startsWith('image/')) {
+            // input要素にファイルをセット
+            dataTransfer.items.add(imgFile);
+            fileInput.files = dataTransfer.files;
+            handleFile(imgFile);
+        } else {
+            alert('画像ファイルを選択してください');
+        }
+    }
+});
+
+dropArea.addEventListener('click', () => {
+    if (window.location.pathname == '/articles/edit/') {
+        fileInput.click();
+    }
+});
