62fd100bcf09174787d8868aca4d19449ed74995
k-azuki-t <teraoctit@gmail.com>
2025-02-21 00:31:56 +0900

    記事詳細ページのレイアウト・お気に入り判定処理追加

--- articles/views.py
+++ articles/views.py
@@ -2,15 +2,18 @@ from django.shortcuts import render
 from django.views.generic import TemplateView, CreateView, DetailView
 from .forms import ContentForm
 from django.urls import reverse_lazy
-from .models import Article
+from .models import Article, Favorite
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.core.files.storage import default_storage
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 # Create your views here.
 
 class ArticleTopView(TemplateView):
     template_name = 'articles/articles_top.html'
+
 
 class ArticleEditView(CreateView):
     model = Article
@@ -25,10 +28,24 @@ class ArticleEditView(CreateView):
         form.instance.author = self.request.user
         return form
     
+
 class ArticleDetailView(DetailView):
     model=Article
     template_name = 'articles/articles_detail.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.user.is_authenticated:
+            user = self.request.user
+            article = self.get_object()  # 表示対象の記事
+            # お気に入り登録されているかをチェック
+            is_favorited = Favorite.objects.filter(user=user, article=article).exists()
+            # コンテキストに追加
+            context["is_favorited"] = is_favorited
+        else:
+            context["is_favorited"] = False
+        return context
+            
 
 @csrf_exempt
 def upload_image(request):
