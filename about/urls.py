8cdef859d4d667e2488d73ab89dae177be291cdf
k-azuki-t <teraoctit@gmail.com>
2025-03-03 23:05:16 +0900

    プライバシーポリシー・利用規約の追加 / その他微修正

--- about/urls.py
+++ about/urls.py
@@ -6,4 +6,6 @@ app_name = 'about'
 urlpatterns = [
     path('kurage/', AboutKurageView.as_view(), name='kurage'),
     path('this_site/', AboutThisSiteView.as_view(), name='this_site'),
+    path('this_site/privacy_policy/', AboutPrivacyPolicyView.as_view(), name='privacy_policy'),
+    path('this_site/terms_of_use/', AboutTermsOfUseView.as_view(), name='terms_of_use'),
 ]
