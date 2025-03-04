413ff1c1f32ee749f06959bb4d5a8cc3fbc9f1f2
k-azuki-t <teraoctit@gmail.com>
2025-03-05 06:02:49 +0900

    エラー内容の日本語化・ユーザー登録画面でのパスワード入力必須化

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,35 +3,47 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm, Password
 from .models import ServiceUser
 from django.core.exceptions import ValidationError
 
+from django.contrib.auth import password_validation
+from django.utils.translation import gettext_lazy as _
+
 
 class CustomUserCreationForm(UserCreationForm):
+
     class Meta:
         model = ServiceUser
-        fields = ['name', 'email']  # 必要なフィールドを指定
+        fields = ['name', 'email', 'password1', 'password2']  # 必要なフィールドを指定
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        password1, password2 = self.create_password_fields()
+        self.fields["password1"] = password1
+        self.fields["password2"] = password2
+    
+    # テンプレート側でのパスワード・確認用パスワードを入力必須化
+    @staticmethod
+    def create_password_fields(label1=_("Password"), label2=_("Password confirmation")):
+        password1 = forms.CharField(
+            label=label1,
+            required=True,
+            strip=False,
+            widget=forms.PasswordInput(attrs={"autocomplete": "new-password"}),
+            help_text=password_validation.password_validators_help_text_html(),
+        )
+        password2 = forms.CharField(
+            label=label2,
+            required=True,
+            widget=forms.PasswordInput(attrs={"autocomplete": "new-password"}),
+            strip=False,
+            help_text=_("Enter the same password as before, for verification."),
+        )
+        return password1, password2
+
+    # メールアドレスのバリデータはUserCreationFormに存在しないため、自身で定義
     def clean_email(self):
         email = self.cleaned_data.get('email')
         if ServiceUser.objects.filter(email=email).exists():
             raise ValidationError('このメールアドレスはすでに登録されています。')
         return email
-    
-    def clean_password1(self):
-        password1 = self.cleaned_data.get('password1')
-        if not password1:
-            raise ValidationError('確認用パスワードを入力してください。')
-        if len(password1) < 8:
-            raise ValidationError('パスワードは8文字以上で設定してください。')
-        return password1
-    
-    def clean_password2(self):
-        password1 = self.cleaned_data.get('password1')
-        password2 = self.cleaned_data.get('password2')
-        print(password1, password2)
-        if not password2:
-            raise ValidationError('確認用パスワードを入力してください。')
-        if password1 is not None and password1 != password2:
-            raise ValidationError('パスワードが一致しません。')
-        return password2
     
 
 class CustomUserChangeForm(UserChangeForm):
