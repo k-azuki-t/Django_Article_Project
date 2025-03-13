8d4c7a36ec8debb0d115587e5090cf3d42977063
k-azuki-t <teraoctit@gmail.com>
2025-03-13 22:32:21 +0900

    パスワードの説明が表示されるよう編集

--- static/js/accounts.js
+++ static/js/accounts.js
@@ -1,1 +1,12 @@
-const passwordInputs = document.querySelectorAll('input[type=password]');
+// パスワードの条件を表示する関数
+function displayPassExplanation() {
+    const container = document.querySelector('.message-container');
+    const message = document.createElement('div')
+    message.setAttribute('class', 'item-container message');
+    message.innerHTML = 'パスワードは以下を満たす必要があります。<br>・メールアドレスと類似しない<br>・8文字以上である<br>・簡易的でない/全て数字ではない';
+
+    container.replaceChildren(message);
+    setTimeout(() => {
+        message.style.display = 'none';
+    }, 4000);
+}
