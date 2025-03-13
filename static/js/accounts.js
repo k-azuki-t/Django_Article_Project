02f744e9b914778b9253d5a74119b99398c16aa4
k-azuki-t <teraoctit@gmail.com>
2025-03-14 00:53:44 +0900

    メッセージの表示制限時間を表す円の追加

--- static/js/accounts.js
+++ static/js/accounts.js
@@ -1,12 +1,31 @@
 // パスワードの条件を表示する関数
 function displayPassExplanation() {
     const container = document.querySelector('.message-container');
-    const message = document.createElement('div')
-    message.setAttribute('class', 'item-container message');
-    message.innerHTML = 'パスワードは以下を満たす必要があります。<br>・メールアドレスと類似しない<br>・8文字以上である<br>・簡易的でない/全て数字ではない';
-
-    container.replaceChildren(message);
+    container.innerHTML = `
+        <div class="item-container message">
+            パスワードは以下を満たす必要があります。<br>
+            ・メールアドレスと類似しない<br>
+            ・8文字以上である<br>
+            ・簡易的でない/全て数字ではない
+            <div class="circle-container">
+            <svg viewBox="0 0 50 50" width="50px" height="50px">
+                <circle
+                cx="25"
+                cy="25"
+                r="15"
+                fill="transparent"
+                stroke="#fff"
+                stroke-width="6"
+                class="time-circle"
+                ></circle>
+            </svg>
+            </div>
+        </div>
+        `;
+    const message = container.querySelector('.message');
+    
     setTimeout(() => {
         message.style.display = 'none';
     }, 4000);
+
 }
