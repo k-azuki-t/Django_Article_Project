2980cf3f1c84c58d7d0e68973a05192ed27b9c3d
k-azuki-t <teraoctit@gmail.com>
2025-03-17 22:03:13 +0900

    about.model追加

--- about/models.py
+++ about/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
 # Create your models here.
+class MyCareer(models.Model):
+    carrer_id         = models.IntegerField(verbose_name='プロダクトID', primary_key=True, auto_created=True)
+    name              = models.CharField(verbose_name='キャリア名', max_length=150, null=False)
+    content1          = models.TextField(verbose_name='キャリア内容1')
+    content2          = models.TextField(verbose_name='キャリア内容2')
+    content3          = models.TextField(verbose_name='キャリア内容3')
+    content4          = models.TextField(verbose_name='キャリア内容4')
+    content5          = models.TextField(verbose_name='キャリア内容5')
+    career_started_at = models.DateField(verbose_name='キャリア開始日', null=False)
+    career_ended_at   = models.DateField(verbose_name='キャリア終了日')
+    created_at        = models.DateField(verbose_name='作成日', auto_now_add=True)
+
+    class Meta:
+        verbose_name='キャリアマスタ'
+        verbose_name_plural = 'キャリアマスタ'
+
+
+class mySkill(models.Model):
+    skill_id         = models.IntegerField(verbose_name='プロダクトID', primary_key=True, auto_created=True)
+    name              = models.CharField(verbose_name='スキル名', max_length=150, null=False)
+    category          = models.TextField(verbose_name='スキルカテゴリ', null=False)
+    created_at        = models.DateField(verbose_name='作成日', auto_now_add=True)
+
+    class Meta:
+        verbose_name='スキルマスタ'
+        verbose_name_plural = 'スキルマスタ'
+
+
+class InterestedDomain(models.Model):
+    domain_id         = models.IntegerField(verbose_name='ドメインID', primary_key=True, auto_created=True)
+    name              = models.CharField(verbose_name='興味のある領域', max_length=150, null=False)
+    created_at        = models.DateField(verbose_name='作成日', auto_now_add=True)
+
+    class Meta:
+        verbose_name='ドメインマスタ'
+        verbose_name_plural = 'ドメインマスタ'
+
+
+class Capabilities(models.Model):
+    capability_id     = models.IntegerField(verbose_name='資格ID', primary_key=True, auto_created=True)
+    name              = models.CharField(verbose_name='資格名', max_length=150, null=False)
+    created_at        = models.DateField(verbose_name='作成日', auto_now_add=True)
+
+    class Meta:
+        verbose_name='資格マスタ'
+        verbose_name_plural = '資格マスタ'
--- product/models.py
+++ product/models.py
@@ -3,10 +3,11 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     product_id     = models.IntegerField(verbose_name='プロダクトID', primary_key=True, auto_created=True)
-    name           = models.CharField(verbose_name='プロダクト名', max_length=150)
-    content        = models.TextField(verbose_name='コンテンツ')
-    header_img_url = models.ImageField(verbose_name='ヘッダー画像格納先', upload_to='images/')
-    created_at      = models.DateField(verbose_name='登録日', auto_now_add=True)
+    name           = models.CharField(verbose_name='プロダクト名', max_length=150, null=False)
+    content        = models.TextField(verbose_name='コンテンツ', null=False)
+    header_img_url = models.ImageField(verbose_name='ヘッダー画像格納先', upload_to='product_images/', null=False)
+    acccess_url    = models.CharField(verbose_name='プロダクト名', max_length=150, null=False)
+    created_at     = models.DateField(verbose_name='登録日', auto_now_add=True, null=False)
 
     class Meta:
         verbose_name='プロダクトマスタ'
