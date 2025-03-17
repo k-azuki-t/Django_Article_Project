bccc727ce331de59725e801ad4e16d123ede205f
k-azuki-t <teraoctit@gmail.com>
2025-03-17 21:21:00 +0900

    admin.site.register変更

--- articles/admin.py
+++ articles/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register([Article, UploadedFileUrl, ArticleTag, Favorite])
+admin.site.register([Article, Favorite])
 
 # change admin site title
 admin.site.site_header = 'あずきの記事管理サイト'
