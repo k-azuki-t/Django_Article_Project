f9f4f895fe02ab073dc5218e2a228cffbe23f817
k-azuki-t <teraoctit@gmail.com>
2025-03-24 08:45:32 +0900

    DEBUG=Falseの時にstaticファイル・mediaファイルの読み込みが開発環境で行われるよう編集（削除必須）

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,6 @@ SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG_ENV')
-print(DEBUG)
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
--- config/urls.py
+++ config/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from articles.views import *
 from django.conf.urls.static import static
+from django.views.static import serve
 from django.conf import settings
 
 urlpatterns = [
@@ -29,7 +30,7 @@ urlpatterns = [
     path('product/', include('product.urls')),
     path('articles/', include('articles.urls')),
     path('markdownx/', include('markdownx.urls')),
-    # path('upload-image/', upload_image, name='upload_image'),
+    # re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATICFILES_DIRS[0]})
 ]
 
 # Add static file serving in debug mode
@@ -37,3 +38,8 @@ urlpatterns += static(
     settings.MEDIA_URL,
     document_root=settings.MEDIA_ROOT
 )
+
+if not settings.DEBUG:
+    # urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+    urlpatterns += [re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATICFILES_DIRS[0]})]
+    urlpatterns += [re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT})]
