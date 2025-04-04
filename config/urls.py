75c161f59968da9fe1182ea8f53aedf518877ab1
k-azuki-t <teraoctit@gmail.com>
2025-04-04 14:41:35 +0900

    DEBUG=Falseの時にローカルでCSSを読み込む処理を削除（必要ないため）

--- config/urls.py
+++ config/urls.py
@@ -32,8 +32,5 @@ urlpatterns = [
     path('markdownx/', include('markdownx.urls')),
 ]
 
-if not settings.DEBUG:
-    urlpatterns += [re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATICFILES_DIRS[0]})]
-    urlpatterns += [re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT})]
-else:
+if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
